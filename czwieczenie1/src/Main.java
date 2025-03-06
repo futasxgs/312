96227521a7e8d6dae77277d012744767bbc16e24
Oleksandr Isaiev <s33295@pjwstk.edu.pl>
2025-03-06 09:36:17 +0100

    Inital project1

--- czwieczenie1/src/Main.java
+++ czwieczenie1/src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+    //432423
         System.out.printf("21");
     }
 }
